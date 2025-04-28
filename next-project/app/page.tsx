1f79af957805e7edbea8c47c2093f1e5c3b8820f
shreyas <thmshreyas@gmail.com>
2025-04-28 19:26:18 +0530

    adding fetch functionality

--- next-project/app/page.tsx
+++ next-project/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
    <main>
     <Link href='/users'>Users</Link>
     <h1>Hello wolrd</h1>
+    <ProductCard />
     </main>
   )
 }
--- next-project/app/users/page.tsx
+++ next-project/app/users/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+interface user{
+    id:number;
+    name:string;
+}
 
-const UsersPage = () => {
+const UsersPage = async() => {
+    const res= await fetch('https://jsonplaceholder.typicode.com/users');
+    const users:user[]= await res.json();
   return (
-    <div> UsersPage</div>
-  )
+    <>
+    <h1>Users</h1>
+    <ul>
+         {
+            users.map(user=><li key={user.id}>{user.name}</li>)
+         }    
+    </ul>
+    </>
+        )
 }
 
 export default UsersPage;
